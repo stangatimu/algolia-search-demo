ac1a2d6e4fc3cfc2ef72ba85d6c4ab96ddfd7aac
stan gatimu <stangatimu@gmail.com>
2019-01-17 23:52:48 +0300

    fixed a routing error

--- api/routes/search.js
+++ api/routes/search.js
@@ -3,7 +3,13 @@ const express = require('express'),
  searchController = require("../controllers/product-search.js");
 
 
+//path can be as:
+//  /search?query='keyword'&page=0&per=12
 
+
+// query= the word/phrase you want to search
+//page = the page you want to search
+//per = how products you want retuned per page, default is 10
  router.get('/',searchController.products_search);
 
  module.exports = router;
